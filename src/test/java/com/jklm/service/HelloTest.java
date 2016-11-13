f2c6f3cfbf750b6e53038c23846f52f1ba5ed5a6
maniv2013 <maniv2013@users.noreply.github.com>
2016-11-13 21:46:42 +0530

    testng add annotate

--- src/test/java/com/jklm/service/HelloTest.java
+++ src/test/java/com/jklm/service/HelloTest.java
@@ -3,14 +3,16 @@
  */
 package com.jklm.service;
 
+import org.testng.annotations.Test;
+
 /**
  * @author DELL
  *
  */
 public class HelloTest {
 
-	
+	@Test
 	public void doStuff() {
-		
+		System.out.println("run");
 	}
 }
